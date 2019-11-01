b8e29e637ccab4d92a08ea936e0c1ee47418d574
Sergio González <andgonzalezmar@unal.edu.co>
2019-11-01 15:02:25 -0500

    Inclusion entidades ciudad y tipo de documento

---
+++ mobile-app/SE1/app/src/main/java/co/edu/unal/se1/dataAccess/model/Email.java
@@ -0,0 +1,10 @@
+package co.edu.unal.se1.dataAccess.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import java.io.Serializable;
+
+@Entity
+public class Email implements Serializable{
+}
